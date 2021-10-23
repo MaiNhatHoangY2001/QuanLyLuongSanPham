872738b80e0be72320ee760cbe0ccc725ed40780
chinh1506 <chinhhoang115@gmail.com>
2021-10-24 00:20:53 +0700

    bo sung giao dien

--- QuanLyLuongSanPham/src/main/java/gui/Gui_QuanLyHoaDon.java
+++ QuanLyLuongSanPham/src/main/java/gui/Gui_QuanLyHoaDon.java
@@ -4,8 +4,12 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
@@ -19,11 +23,31 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.SwingConstants;
+
 public class Gui_QuanLyHoaDon extends JFrame {
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtTimKiem;
+	private JTextField txtTongTien;
 	private JTable table_1;
-	private JTable table;
+	private JTable tblHoaDon;
+	private JLabel lblTenKhachHang;
+	private JLabel lblMaKhachHang;
+	private JLabel lblNgaySinh;
+	private JLabel lblDiaChi;
+	private JLabel lblSdt;
+	private JButton btnTimKiem;
+	private JComboBox cboTimKiem;
+	private JButton btnThemHoaDon;
+	private JButton btnXemChiTiet;
+	private JButton btnXoa;
+	private JComboBox cboLoaiHoaDon;
+	private JButton btnNgayLap;
+	private JLabel lblNguoiSuDung;
+	private JButton btnMenu;
+	private JLabel lblGio;
+	private JDateChooser txtNgayLap;
 
 	/**
 	 * Launch the application.
@@ -49,6 +73,7 @@ public class Gui_QuanLyHoaDon extends JFrame {
 		getContentPane().setLayout(null);
 
 		JPanel panel = new JPanel();
+		panel.setBorder(null);
 		panel.setBackground(new Color(242, 129, 25));
 		panel.setBounds(0, 0, 1424, 154);
 		getContentPane().add(panel);
@@ -67,18 +92,27 @@ public class Gui_QuanLyHoaDon extends JFrame {
 		lblNewLabel_5.setBounds(1372, 27, 52, 45);
 		panel.add(lblNewLabel_5);
 
-		JLabel lblNewLabel_6 = new JLabel("Chinh");
-		lblNewLabel_6.setToolTipText("Đã đăng nhập");
-		lblNewLabel_6.setForeground(Color.WHITE);
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 36));
-		lblNewLabel_6.setBounds(1240, 27, 161, 45);
-		panel.add(lblNewLabel_6);
+		lblNguoiSuDung = new JLabel("Chinh");
+		lblNguoiSuDung.setHorizontalTextPosition(SwingConstants.RIGHT);
+		lblNguoiSuDung.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNguoiSuDung.setToolTipText("Đã đăng nhập");
+		lblNguoiSuDung.setForeground(Color.WHITE);
+		lblNguoiSuDung.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		lblNguoiSuDung.setBounds(1229, 27, 136, 45);
+		panel.add(lblNguoiSuDung);
 
-		JLabel lblNewLabel_7 = new JLabel("12:00");
-		lblNewLabel_7.setForeground(Color.WHITE);
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 36));
-		lblNewLabel_7.setBounds(1320, 83, 94, 35);
-		panel.add(lblNewLabel_7);
+		lblGio = new JLabel("12:00");
+		lblGio.setForeground(Color.WHITE);
+		lblGio.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		lblGio.setBounds(1320, 83, 94, 35);
+		panel.add(lblGio);
+
+		btnMenu = new JButton("");
+		btnMenu.setToolTipText("Mở/Đóng menu");
+		btnMenu.setBorder(null);
+		btnMenu.setIcon(new ImageIcon("src/main/resources/images/img_bill/menu.PNG"));
+		btnMenu.setBounds(27, 27, 72, 83);
+		panel.add(btnMenu);
 
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(194, 93, 0));
@@ -86,22 +120,25 @@ public class Gui_QuanLyHoaDon extends JFrame {
 		getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBackground(new Color(233, 180, 46));
-		comboBox.setForeground(Color.WHITE);
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 36));
-		comboBox.setModel(new DefaultComboBoxModel(
+		cboLoaiHoaDon = new JComboBox();
+		cboLoaiHoaDon.setBackground(new Color(233, 180, 46));
+		cboLoaiHoaDon.setForeground(Color.WHITE);
+		cboLoaiHoaDon.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		cboLoaiHoaDon.setModel(new DefaultComboBoxModel(
 				new String[] { "H\u00F3a \u0111\u01A1n b\u00E1n", "H\u00F3a \u0111\u01A1n nh\u1EADp" }));
-		comboBox.setSelectedIndex(1);
-		comboBox.setBounds(30, 20, 282, 54);
-		panel_1.add(comboBox);
+		cboLoaiHoaDon.setSelectedIndex(1);
+		cboLoaiHoaDon.setBounds(30, 20, 282, 54);
+		panel_1.add(cboLoaiHoaDon);
 
-		JButton btnNewButton = new JButton("12/03/2021");
-		btnNewButton.setBackground(new Color(233, 180, 46));
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 36));
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBounds(336, 20, 282, 54);
-		panel_1.add(btnNewButton);
+	
+		
+		txtNgayLap= new JDateChooser(new Date());
+		txtNgayLap.setBackground(new Color(233, 180, 46));
+		txtNgayLap.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		txtNgayLap.setForeground(Color.WHITE);
+		txtNgayLap.setBounds(336, 20, 282, 54);
+		txtNgayLap.setDateFormatString("dd/mm/yyyy");
+		panel_1.add(txtNgayLap);
 
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(new Color(242, 129, 25));
@@ -116,10 +153,10 @@ public class Gui_QuanLyHoaDon extends JFrame {
 		scrollPane.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		panel_2.add(scrollPane);
 
-		table = new JTable();
-		table.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		table.setRowHeight(52);
-		table.setModel(new DefaultTableModel(
+		tblHoaDon = new JTable();
+		tblHoaDon.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		tblHoaDon.setRowHeight(52);
+		tblHoaDon.setModel(new DefaultTableModel(
 				new Object[][] { { null, null, null, null, null, null, null },
 						{ null, null, null, null, null, null, null }, { null, null, null, null, null, null, null },
 						{ null, null, null, null, null, null, null }, { null, null, null, null, null, null, null },
@@ -127,7 +164,7 @@ public class Gui_QuanLyHoaDon extends JFrame {
 						{ null, null, null, null, null, null, null }, },
 				new String[] { "M\u00E3 h\u00F3a \u0111\u01A1n", "S\u1ED1 l\u01B0\u1EE3ng", "Ng\u00E0y l\u1EADp",
 						"Khuy\u1EBFn m\u00E3i", "Thu\u1EBF", "T\u00EAn nh\u00E2n vi\u00EAn", "Th\u00E0nh ti\u1EC1n" }));
-		scrollPane.setViewportView(table);
+		scrollPane.setViewportView(tblHoaDon);
 
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(new Color(242, 129, 25));
@@ -147,96 +184,140 @@ public class Gui_QuanLyHoaDon extends JFrame {
 		lblNewLabel_2.setBounds(47, 51, 190, 181);
 		panel_4.add(lblNewLabel_2);
 
-		JLabel lblNewLabel_3 = new JLabel("Hoang Van Chinh");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblNewLabel_3.setBounds(47, 259, 211, 42);
-		panel_4.add(lblNewLabel_3);
+		lblTenKhachHang = new JLabel("Hoang Van Chinh");
+		lblTenKhachHang.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblTenKhachHang.setBounds(47, 259, 211, 42);
+		panel_4.add(lblTenKhachHang);
 
-		JLabel lblNewLabel_3_1 = new JLabel("M\u00E3 Kh\u00E1ch H\u00E0ng: KH123");
-		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblNewLabel_3_1.setBounds(268, 60, 345, 35);
-		panel_4.add(lblNewLabel_3_1);
+		lblMaKhachHang = new JLabel("M\u00E3 Kh\u00E1ch H\u00E0ng: KH123");
+		lblMaKhachHang.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblMaKhachHang.setBounds(268, 60, 345, 35);
+		panel_4.add(lblMaKhachHang);
 
-		JLabel lblNewLabel_3_2 = new JLabel("S\u1ED1 \u0111i\u1EC7n tho\u1EA1i: 0967127083");
-		lblNewLabel_3_2.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblNewLabel_3_2.setBounds(268, 125, 356, 35);
-		panel_4.add(lblNewLabel_3_2);
+		lblSdt = new JLabel("S\u1ED1 \u0111i\u1EC7n tho\u1EA1i: 0967127083");
+		lblSdt.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblSdt.setBounds(268, 125, 356, 35);
+		panel_4.add(lblSdt);
 
-		JLabel lblNewLabel_3_3 = new JLabel("\u0110\u1ECBa ch\u1EC9: \u0110\u0103k N\u00F4ng");
-		lblNewLabel_3_3.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblNewLabel_3_3.setBounds(268, 191, 368, 35);
-		panel_4.add(lblNewLabel_3_3);
+		lblDiaChi = new JLabel("\u0110\u1ECBa ch\u1EC9: \u0110\u0103k N\u00F4ng");
+		lblDiaChi.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblDiaChi.setBounds(268, 191, 368, 35);
+		panel_4.add(lblDiaChi);
 
-		JLabel lblNewLabel_3_4 = new JLabel("Ng\u00E0y sinh: 15/06/2001");
-		lblNewLabel_3_4.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblNewLabel_3_4.setBounds(268, 259, 356, 35);
-		panel_4.add(lblNewLabel_3_4);
+		lblNgaySinh = new JLabel("Ng\u00E0y sinh: 15/06/2001");
+		lblNgaySinh.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblNgaySinh.setBounds(268, 259, 356, 35);
+		panel_4.add(lblNgaySinh);
 
 		JPanel panel_4_1 = new JPanel();
 		panel_4_1.setBackground(new Color(248, 198, 153));
 		panel_4_1.setBounds(723, 0, 676, 350);
 		panel_3.add(panel_4_1);
 
-		JButton btnThmHan = new JButton("Th\u00EAm h\u00F3a \u0111\u01A1n");
-		btnThmHan.setBounds(24, 23, 193, 44);
-		btnThmHan.addActionListener(new ActionListener() {
+		btnThemHoaDon = new JButton("Th\u00EAm h\u00F3a \u0111\u01A1n");
+		btnThemHoaDon.setBounds(24, 23, 193, 44);
+		btnThemHoaDon.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		panel_4_1.setLayout(null);
-		btnThmHan.setForeground(Color.WHITE);
-		btnThmHan.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnThmHan.setBackground(new Color(233, 180, 46));
-		panel_4_1.add(btnThmHan);
+		btnThemHoaDon.setForeground(Color.WHITE);
+		btnThemHoaDon.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnThemHoaDon.setBackground(new Color(233, 180, 46));
+		panel_4_1.add(btnThemHoaDon);
 
-		JButton btnTmKim = new JButton("T\u00ECm ki\u1EBFm");
-		btnTmKim.setBounds(24, 114, 193, 44);
-		btnTmKim.setForeground(Color.WHITE);
-		btnTmKim.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnTmKim.setBackground(new Color(233, 180, 46));
-		panel_4_1.add(btnTmKim);
+		btnTimKiem = new JButton("T\u00ECm ki\u1EBFm");
+		btnTimKiem.setBounds(24, 114, 193, 44);
+		btnTimKiem.setForeground(Color.WHITE);
+		btnTimKiem.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnTimKiem.setBackground(new Color(233, 180, 46));
+		panel_4_1.add(btnTimKiem);
 
-		JButton btnXemChiTit = new JButton("Xem chi ti\u1EBFt");
-		btnXemChiTit.setBounds(247, 23, 177, 44);
-		btnXemChiTit.setForeground(Color.WHITE);
-		btnXemChiTit.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnXemChiTit.setBackground(new Color(233, 180, 46));
-		panel_4_1.add(btnXemChiTit);
+		btnXemChiTiet = new JButton("Xem chi ti\u1EBFt");
+		btnXemChiTiet.setBounds(247, 23, 177, 44);
+		btnXemChiTiet.setForeground(Color.WHITE);
+		btnXemChiTiet.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnXemChiTiet.setBackground(new Color(233, 180, 46));
+		panel_4_1.add(btnXemChiTiet);
 
-		JButton btnXa = new JButton("X\u00F3a");
-		btnXa.setBounds(460, 23, 177, 44);
-		btnXa.setForeground(Color.WHITE);
-		btnXa.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnXa.setBackground(new Color(233, 180, 46));
-		panel_4_1.add(btnXa);
+		btnXoa = new JButton("X\u00F3a");
+		btnXoa.setBounds(460, 23, 177, 44);
+		btnXoa.setForeground(Color.WHITE);
+		btnXoa.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnXoa.setBackground(new Color(233, 180, 46));
+		panel_4_1.add(btnXoa);
 
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(
+		cboTimKiem = new JComboBox();
+		cboTimKiem.setModel(new DefaultComboBoxModel(
 				new String[] { "T\u00ECm theo m\u00E3 h\u00F3a \u0111\u01A1n", "T\u00ECm theo ng\u00E0y l\u1EADp",
 						"T\u00ECm theo m\u00E3 kh\u00E1ch h\u00E0ng", "T\u00ECm theo m\u00E3 s\u1EA3n ph\u1EA9m" }));
-		comboBox_1.setSelectedIndex(0);
-		comboBox_1.setForeground(Color.WHITE);
-		comboBox_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		comboBox_1.setBackground(new Color(233, 180, 46));
-		comboBox_1.setBounds(24, 207, 613, 44);
-		panel_4_1.add(comboBox_1);
+		cboTimKiem.setSelectedIndex(0);
+		cboTimKiem.setForeground(Color.WHITE);
+		cboTimKiem.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		cboTimKiem.setBackground(new Color(233, 180, 46));
+		cboTimKiem.setBounds(24, 207, 613, 44);
+		panel_4_1.add(cboTimKiem);
 
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		textField.setBounds(247, 114, 390, 44);
-		panel_4_1.add(textField);
-		textField.setColumns(10);
+		txtTimKiem = new JTextField();
+		txtTimKiem.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtTimKiem.setBounds(247, 114, 390, 44);
+		panel_4_1.add(txtTimKiem);
+		txtTimKiem.setColumns(10);
 
 		JLabel lblNewLabel_4 = new JLabel("T\u1ED5ng ti\u1EC1n: ");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_4.setBounds(65, 277, 157, 44);
 		panel_4_1.add(lblNewLabel_4);
 
-		textField_1 = new JTextField();
-		textField_1.setEnabled(false);
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		textField_1.setColumns(10);
-		textField_1.setBounds(247, 278, 390, 44);
-		panel_4_1.add(textField_1);
+		txtTongTien = new JTextField();
+		txtTongTien.setEnabled(false);
+		txtTongTien.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtTongTien.setColumns(10);
+		txtTongTien.setBounds(247, 278, 390, 44);
+		panel_4_1.add(txtTongTien);
+		JPanel pnMenu = new Menu();
+		Rectangle temp = panel.getBounds();
+		Rectangle temp1 = panel_1.getBounds();
+		Rectangle temp2 = panel_2.getBounds();
+		Rectangle temp3 = panel_3.getBounds();
+
+		setGio(lblGio);
+		btnMenu.addActionListener(e -> {
+
+			if (panel.getX() == 400) {
+
+				this.remove(pnMenu);
+				panel.setBounds(temp);
+				panel_1.setBounds(temp1);
+				panel_2.setBounds(temp2);
+				panel_3.setBounds(temp3);
+
+			} else {
+				panel.setBounds(400, 0, 1440 - 400, panel.getHeight());
+				panel_1.setBounds(400, panel_1.getY(), 1440 - 400, panel_1.getHeight());
+				panel_2.setBounds(400, panel_2.getY(), 1440 - 400, panel_2.getHeight());
+				panel_3.setBounds(400, panel_3.getY(), 1440 - 400, panel_3.getHeight());
+				add(pnMenu);
+			}
+		});
+	}
+
+	/**
+	 * chỉnh giờ cho lable
+	 * @param lblGio
+	 */
+	public static void setGio(JLabel lblGio) {
+		TimerTask timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				lblGio.setText((new Date().getHours() >= 10 ? "" : "0") + (new Date().getHours())
+						+ ((new Date().getSeconds() % 2) != 0 ? " " : ":")
+						+ ((new Date().getMinutes() >= 10 ? "" : "0") + (new Date().getMinutes())));
+			}
+
+		};
+		long delay = 1000L;
+		Timer timer = new Timer("Timer");
+		timer.schedule(timerTask, 0, delay);
 	}
 }
