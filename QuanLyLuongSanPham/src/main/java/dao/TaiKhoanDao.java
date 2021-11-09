bd95c390b4e0bd9afe422192facbccf8e787edaa
Madnok <hungviper016@gmail.com>
2021-11-09 19:16:56 +0700

    bo sung giao dien qltk

--- QuanLyLuongSanPham/src/main/java/dao/TaiKhoanDao.java
+++ QuanLyLuongSanPham/src/main/java/dao/TaiKhoanDao.java
@@ -1,11 +1,14 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import hibernateCfg.HibernateConfig;
-
+import model.NhanVien;
 import model.TaiKhoan;
 
 public class TaiKhoanDao {
@@ -24,6 +27,26 @@ public class TaiKhoanDao {
 		}
 		session.close();
 		return true;
-
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public List<TaiKhoan> getDsTaiKhoan() {
+		List<TaiKhoan> list = new ArrayList<TaiKhoan>();
+		Session session = sessionFactory.getCurrentSession();
+		Transaction tr = session.getTransaction();
+		try {
+			tr.begin();
+			list = session.createNativeQuery("select * from TaiKhoan", TaiKhoan.class).getResultList();
+			tr.commit();
+			System.out.println(list);
+			return list;
+		} catch (Exception e) {
+			tr.rollback();
+		} finally {
+			session.close();
+		}
+		return null;
 	}
 }
